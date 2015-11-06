a93e08adfbda56516cee6d75a239f70c0801b3e4
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-06 15:13:35 -0500

    search

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,1 +1,10 @@
 console.log('script.js is linked')
+
+$('#nachos').on('click', function(e){
+  location.href = "/data?tag=nachos"
+})
+
+$('#kittens').on('click', function(e){
+  location.href = "/data?tag=kittens"
+})
+
